236b6cb59e6e0bf24184a7e6ee67f2081960f807
KeiyaKano <keiyasasaki08250825@gamil.com>
2025-05-10 08:17:20 +0900

    20250510_tailwind.config.js修正対応

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './app/**/*.{ts,tsx,js,jsx}',
-    './pages/**/*.{ts,tsx,js,jsx}',
-    './components/**/*.{ts,tsx,js,jsx}',
-  ],
+  content: ['./src/**/*.{ts,tsx,js,jsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
 export default config;
