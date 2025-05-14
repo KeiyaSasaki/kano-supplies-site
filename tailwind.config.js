1e5be55acb3a215f241c090df99c3e230c10426f
KeiyaKano <keiyasasaki08250825@gamil.com>
2025-05-14 11:25:16 +0900

    20250514_HP画面修正対応(サービスページ作成等）

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   content: [
     './app/**/*.{ts,tsx,js,jsx}',
+    './pages/**/*.{ts,tsx,js,jsx}',
     './components/**/*.{ts,tsx,js,jsx}',
   ],
   theme: {
